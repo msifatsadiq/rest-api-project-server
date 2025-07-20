bf247151122dd78300525d6f60aded518886e0a7
msifatsadiq <msifatsadiq@gmail.com>
2025-07-20 12:40:47 +0600

    adding the rate limitter function using upstash and redis

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import routes from './Routes/noteRoutes.js';
 import { connectDB } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -9,15 +10,20 @@ dotenv.config();
 const app = express();
 const PORT =  process.env.PORT || 5000; 
 
-connectDB()
 
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(rateLimiter)
 
 app.use("/api/notes",routes)
+
+connectDB().then(()=>{
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+})
+
+
 
